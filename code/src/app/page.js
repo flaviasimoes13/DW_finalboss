4f009dd29dc3d789ef85bcb7f888900806c3b553
claraarigomes <claraarigomes@gmail.com>
2024-12-15 17:15:51 +0000

    conflits in git fixed

--- code/src/app/page.js
+++ code/src/app/page.js
@@ -8,46 +8,163 @@ import Recipe from "./components/Recipe/Recipe";
 export default function Home() {
   return (
     <>
-            <section className={Styles.highlightItem}>
-              <h2 className={Styles.sectionTitle}>Featured Recipe</h2>
-              <Image src="/NOOOOOO.jpg" alt="Featured Recipe" width={300} height={200}/>
-              <h3 className={Styles.highlightItemTitle}>Recipe Name</h3>
-              <p className={Styles.highlightItemDescription}>Recipe description</p>
-            </section>
+      <section className={Styles.highlightItem}>
+        <div className={Styles.highlightContent}>
+          <h2 className={Styles.sectionTitle}>Featured Recipe</h2>
+          <h3 className={Styles.highlightItemTitle}>Recipe Name</h3>
+          <p className={Styles.highlightItemDescription}>Recipe description</p>
+        </div>
+        <Image
+          src="/NOOOOOO.jpg"
+          alt="Featured Recipe"
+          width={300}
+          height={200}
+        />
+      </section>
 
-          <section className={Styles.quickRecipes}>
-            <h2 className={Styles.sectionTitle}>Quick Recipes</h2>
-            <div className={Styles.quickRecipesItem}>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/> 
-            </div>
-          </section>
-          
-          <section className={Styles.myRecipes}>
-            <h2 className={Styles.sectionTitle}>My Recipes</h2>
-            <div className={Styles.myRecipesItem}>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-              <Recipe src='/capivarateste.webp' alt='yummy capivara' name='capi' category='CARNE'/>
-            </div>
-          </section>
+      <section className={Styles.quickRecipes}>
+        <h2 className={Styles.sectionTitle}>Quick Recipes</h2>
+        <div className={Styles.quickRecipesItem}>
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+        </div>
+      </section>
+
+      <section className={Styles.myRecipes}>
+        <h2 className={Styles.sectionTitle}>My Recipes</h2>
+        <div className={Styles.myRecipesItem}>
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+          <Recipe
+            src="/capivarateste.webp"
+            alt="yummy capivara"
+            name="capi"
+            category="CARNE"
+          />
+        </div>
+      </section>
     </>
   );
 }
